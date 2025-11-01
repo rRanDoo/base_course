616c1d69c27774a6677622a0f858e19c90cb43bc
rRanDoo <alisyata14@gmail.com>
2025-11-01 13:39:42 +0000

    с лабами

---
+++ lab2/lab2_dop_1.py
@@ -0,0 +1,14 @@
+print('a*(x**2)+b*x+c=0:')
+a=int(input('Введите коэффицент a: '))
+b=int(input('Введите коэффицент b: '))
+c=int(input('Введите коэффицент c: '))
+D= (b**2)-4*a*c
+if D>0:
+    x1=((-b)+D)/2
+    x2=((-b)-D)/2
+    print(x1,x2)
+elif D==0:
+    x=(-b)/2
+    print(x)
+else:
+    print('Корней нет')
