3e3bbfe9d46869d5fcc31182e783301d248d7241
rRanDoo <alisyata14@gmail.com>
2025-11-22 11:10:57 +0000

    лекция 3

---
+++ less3_np-array.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+a = [1, 2, 4]
+
+b = np.array(a)  # Создание массива из списка
+
+print(type(a))
+print(type(b))
+
+print(b * b)
+print(b / b)
+print(b - b)
+
+# print(a - a)
+
+print(b[-1])
