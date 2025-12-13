8c944d96d6d44d3b4cd83e33dcf2ee239e24c074
rRanDoo <alisyata14@gmail.com>
2025-12-13 13:27:01 +0000

    ww32

---
+++ neyawn_zad.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def circle_plotter(radius=10):
+
+    x = np.arange(-2*radius, 2*radius, 14)
+    y = np.arange(-2*radius, 2*radius, 16)
+
+    # Переход к неявнозаданным координатам
+    X, Y = np.meshgrid(x, y)
+
+    fxy = X**2 + Y**2 - radius**2  # Уравнение окружности
+
+    # Команда рисования
+    plt.contour(X, Y, fxy, levels=[0])
+    plt.axis('equal')
+
+    plt.savefig('fig_4.png')
+
+
+if __name__ == '__main__':
+    circle_plotter()
